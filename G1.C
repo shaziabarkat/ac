ce0e65787cdeb60682b6089202aa914b4fbe3c61
shaziabarkat <35466531+shaziabarkat@users.noreply.github.com>
2018-01-22 21:25:01 +0530

    Add files via upload

---
+++ G1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include <math.h>
+#include<conio.h>
+int main()
+{
+
+  double a,b,result;
+  printf("enter the no:");
+  scanf("%lf",&a);
+  printf("the power no.: ");
+  scanf("%lf",&b);
+  printf("the result is %lf",pow(a,b));
+  getch();
+}
